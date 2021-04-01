442afb0a85c407df03b33b5bd70b47de39c881a9
Chris <chrisatmachine@gmail.com>
2021-04-01 02:16:36 -0400

    update path

--- config.def.h
+++ config.def.h
@@ -46,7 +46,7 @@ static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 
 #define FORCE_VSPLIT 1  /* nrowgrid layout: force two clients to always split vertically */
-#include "vanitygaps.c"
+#include "./patches/vanitygaps.c"
 
 /* #include "./patches/fibonacci.c" */
 static const Layout layouts[] = {
