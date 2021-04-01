6e1deb5c489f24f99e201863710f1c028ee1c71c
Chris <chrisatmachine@gmail.com>
2021-04-01 02:48:30 -0400

    now with a systray

--- config.def.h
+++ config.def.h
@@ -4,11 +4,15 @@
 /* appearance */
 static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
+static const unsigned int systraypinning = 1;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayspacing = 2;   /* systray spacing */
+static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+static const int showsystray        = 1;     /* 0 means no systray */
+static const unsigned int gappih    = 14;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 14;       /* vert inner gap between windows */
 static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov    = 30;       /* vert outer gap between windows and screen edge */
-static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
+static       int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Hack Regular Nerd Font Complete:size=12" };
@@ -106,8 +110,8 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_f,      setlayout,      {.v = &layouts[1]} }, // float
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} }, // monocle
     { MODKEY|ShiftMask,             XK_t,      setlayout,      {.v = &layouts[4]} }, // dwindle
-+	{ MODKEY,                       XK_g,      togglegaps,     {0} },
-+	{ MODKEY|ShiftMask,             XK_g,      defaultgaps,    {0} },
+	{ MODKEY,                       XK_g,      togglegaps,     {0} },
+	{ MODKEY|ShiftMask,             XK_g,      defaultgaps,    {0} },
 	/* { MODKEY|ShiftMask,			    XK_t,      spawn,		   SHCMD("kitty -e ytop") }, */
     /* { MODKEY|ShiftMask,             XK_y,      setlayout,      {.v = &layouts[3]} }, // spiral */
 	/* { MODKEY,                       XK_semicolon,      shiftview,      {.i = 1 } }, */
