69e527b343278663fcbf43d8dd5b46a8c8ad930d
Chris <chrisatmachine@gmail.com>
2021-04-01 15:24:37 -0400

    media keys

--- config.def.h
+++ config.def.h
@@ -16,6 +16,9 @@ static       int smartgaps          = 1;        /* 1 means no outer gap when the
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Hack Regular Nerd Font Complete:size=12" };
+static const char *upvol[]   = { "/usr/bin/pactl", "set-sink-volume", "0", "+5%",     NULL };
+static const char *downvol[] = { "/usr/bin/pactl", "set-sink-volume", "0", "-5%",     NULL };
+static const char *mutevol[] = { "/usr/bin/pactl", "set-sink-mute",   "0", "toggle",  NULL };
 static const char dmenufont[]       = "Hack Regular Nerd Font Complete:size=12";
 static const char col_gray1[]       = "#1E1E1E";
 static const char col_gray2[]       = "#444444";
@@ -86,6 +89,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "kitty", NULL };
 
+#include <X11/XF86keysym.h>
 #include "./patches/shiftview.c"
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -115,6 +119,9 @@ static Key keys[] = {
     { MODKEY|ShiftMask,             XK_t,      setlayout,      {.v = &layouts[4]} }, // dwindle
 	{ MODKEY,                       XK_g,      togglegaps,     {0} },
 	{ MODKEY|ShiftMask,             XK_g,      defaultgaps,    {0} },
+	{ 0,                            XF86XK_AudioLowerVolume, spawn, {.v = downvol } },
+	{ 0,                            XF86XK_AudioMute, spawn, {.v = mutevol } },
+	{ 0,                            XF86XK_AudioRaiseVolume, spawn, {.v = upvol   } },
 	/* { MODKEY|ShiftMask,			    XK_t,      spawn,		   SHCMD("kitty -e ytop") }, */
     /* { MODKEY|ShiftMask,             XK_y,      setlayout,      {.v = &layouts[3]} }, // spiral */
 	/* { MODKEY,                       XK_semicolon,      shiftview,      {.i = 1 } }, */
