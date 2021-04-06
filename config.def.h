2fe3c88a8f2bd096557a85f23b33593eb75b1dbc
christianchiarulli <chrisatmachine@gmail.com>
2021-04-06 03:07:08 -0400

    updates

--- config.def.h
+++ config.def.h
@@ -5,43 +5,45 @@
 static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int systraypinning = 2;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
-static const unsigned int systrayspacing = 2;   /* systray spacing */
+static const unsigned int systrayspacing = 3;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
 static const int showsystray        = 1;     /* 0 means no systray */
-static const unsigned int gappih    = 14;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 14;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 30;       /* vert outer gap between windows and screen edge */
-static       int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
+static const unsigned int gappih    = 23;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 23;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 23;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 23;       /* vert outer gap between windows and screen edge */
+static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-/* static const char *fonts[]          = { "Hack Regular Nerd Font Complete:size=12", "JoyPixels:pixelsize=12:antialias=true:autohint=true" }; */
-static const char *fonts[]          = { "monospace:size=10", "JoyPixels:pixelsize=10" };
+static const char *fonts[]          = { "Hack Regular Nerd Font Complete:size=12", "JoyPixels:pixelsize=12:antialias=true:autohint=true" };
 static const char *upvol[]   = { "/usr/bin/pactl", "set-sink-volume", "@DEFAULT_SINK@", "+5%",     NULL };
 static const char *downvol[] = { "/usr/bin/pactl", "set-sink-volume", "@DEFAULT_SINK@", "-5%",     NULL };
 static const char *mutevol[] = { "/usr/bin/pactl", "set-sink-mute",   "@DEFAULT_SINK@", "toggle",  NULL };
-static const char dmenufont[]       = "Hack Regular Nerd Font Complete:size=12";
-static const char col_gray1[]       = "#1e1e1e";
-static const char col_gray2[]       = "#444444";
+static const char col_gray1[]       = "#232731";
+static const char col_gray2[]       = "#333333";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
+static const char col_purple[]      = "#5a5aa4";
 static const char col_red[]         = "#d54646";
 static const char col_green[]       = "#23d18b";
 static const char col_yellow[]      = "#d7ba7d";
-static const char col_blue[]        = "#569cd6";
-static const char col_magenta[]       = "#c586c0";
-static const char col_white[]         = "#abb2bf";
+static const char col_blue[]        = "#81a1c1";
+static const char col_magenta[]     = "#c586c0";
+static const char col_white[]       = "#abb2bf";
+static const char col_bg_alt[]      = "#292d38";
 
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray4, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel]  = { col_gray4, col_blue,  col_blue  },
 	[SchemeStatus]  = { col_gray4, col_gray1,  "#000000"  }, // Statusbar right {text,background,not used but cannot be empty}
-	[SchemeTagsSel]  = { col_gray4, col_cyan,  "#000000"  }, // Tagbar left selected {text,background,not used but cannot be empty}
+
+	[SchemeTagsSel]  = { col_gray3, col_blue,  "#000000"  }, // Tagbar left selected {text,background,not used but cannot be empty}
     [SchemeTagsNorm]  = { col_gray4, col_gray1,  "#000000"  }, // Tagbar left unselected {text,background,not used but cannot be empty}
-    [SchemeInfoSel]  = { col_gray4, col_cyan,  "#000000"  }, // infobar middle  selected {text,background,not used but cannot be empty}
-    [SchemeInfoNorm]  = { col_gray4, col_gray1,  "#000000"  }, // infobar middle  unselected {text,background,not used but cannot be empty}
+
+    [SchemeInfoSel]  = { col_gray3, col_bg_alt,  "#000000"  }, // infobar middle  selected {text,background,not used but cannot be empty}
+    [SchemeInfoNorm]  = { col_gray3, col_gray1,  "#000000"  }, // infobar middle  unselected {text,background,not used but cannot be empty}
 };
 
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
@@ -75,13 +77,13 @@ static const int resizehints = 1;    /* 1 means respect size hints in tiled resi
 static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "[]=",      tile },    /* first entry is default */
-	{ "H[]",      deck },
+    { "[\\]",     dwindle },
 	{ "[M]",      monocle },
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "|M|",      centeredmaster },
 	{ ">M>",      centeredfloatingmaster },
 	{ NULL,       NULL },
- 	/* { "[\\]",     dwindle }, */
+    /* { "H[]",      deck },*/
 	/* { "TTT",      bstack }, */
  	/* { "[@]",      spiral }, */
 };
@@ -98,8 +100,6 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
-static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "kitty", NULL };
 
 #include <X11/XF86keysym.h>
@@ -118,7 +118,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY|ShiftMask,             XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_q,      killclient,     {0} },
-	{ MODKEY,                       XK_space,  spawn,          {.v = dmenucmd } },
+	{ MODKEY,                       XK_space,  spawn,          SHCMD("dmenu_run") },
 	{ MODKEY|ShiftMask,             XK_space,  setlayout,      {0} }, 
 	{ MODKEY,			            XK_w,      spawn,		   SHCMD("$BROWSER") },
 	{ MODKEY,			            XK_r,      spawn,		   SHCMD("kitty -e ranger") },
@@ -161,10 +161,6 @@ static Key keys[] = {
 	/* { MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } }, */
 };
 
-/* # Pulse Audio controls */
-/* bindsym XF86AudioRaiseVolume exec --no-startup-id pactl set-sink-volume @DEFAULT_SINK@ +10% #increase sound volume */
-/* bindsym XF86AudioLowerVolume exec --no-startup-id pactl set-sink-volume @DEFAULT_SINK@ -10% #decrease sound volume */
-/* bindsym XF86AudioMute        exec --no-startup-id pactl set-sink-mute @DEFAULT_SINK@ toggle # mute sound */
 
 /* bindsym $mod+F1		        restart */
 /* bindsym $mod+F2		        exec --no-startup-id screenkey -s small --scr 1 -p fixed -g 600x100+2573+1330 --opacity .9 --font-color white */
